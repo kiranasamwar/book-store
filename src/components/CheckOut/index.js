e4b8119a2dd756a609dba984d25a4260f981991c
kiranasamwar <kiranasamwar@gmail.com>
2024-04-30 18:25:15 +0530

    Add routes to the app

---
+++ src/components/CheckOut/index.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class CheckOut extends Component{
+    render(){
+        return(
+            <div>CheckOut</div>
+        )
+    }
+}
+
+export default CheckOut
